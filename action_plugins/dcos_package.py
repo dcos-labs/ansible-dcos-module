06d8fe94829ba01dc761dc89298bb9448bdfd59b
Dirk Jonker <dirk.jonker@lunatech.com>
2018-02-26 17:31:40 +0100

    Change dcos_package into an action plugin

---
+++ action_plugins/dcos_package.py
@@ -0,0 +1,125 @@
+"""
+Action plugin to configure a DC/OS cluster.
+Uses the Ansible host to connect directly to DC/OS.
+"""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+from ansible.errors import AnsibleActionFail
+from ansible.module_utils.six import PY2
+#from ansible.utils.vars import merge_hash
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+try:
+    import dcos
+    import dcos.package
+    from dcos.errors import DCOSException
+    HAS_DCOS = True
+except ImportError:
+    HAS_DCOS = False
+
+
+def _version(v):
+    return tuple(map(int, v.split('.')))
+
+
+def _ensure_dcos():
+    """Check whether the dcos[cli] package is installed."""
+    if PY2:
+        raise AnsibleActionFail("Python 3 is required for DC/OS")
+
+    if not HAS_DCOS:
+        raise AnsibleActionFail(msg="Package `dcoscli` is not installed, try `pip install dcoscli`")
+    else:
+        v = _version(dcos.version)
+        if v < (0, 5, 0):
+            raise AnsibleActionFail("dcos 0.5.x is required, found {}".format(dcos.version))
+        if v >= (0, 6, 0):
+            raise AnsibleActionFail("dcos cli version > 0.5.x detected, may not work")
+    display.vvv("dcos: all prerequisites seem to be in order")
+
+
+def get_current_version(pm, package, app_id):
+    packages = {p['name']: p['version'] for p in pm.installed_apps(package, app_id)}
+    display.vvv('packages: {}'.format(packages))
+    v = packages.get(package)
+    display.vvv('{} current version: {}'.format(package, v))
+    return v
+
+
+def get_wanted_version(version, state):
+    if state == 'absent':
+        return None
+    return version
+
+
+def install_package(pm, package, version, options=None):
+    display.vvv("DC/OS: installing package {} version {}"
+                .format(package, version))
+    display.vvv("options: {}".format(options))
+    pkg = pm.get_package_version(package, version)
+    display.vvv('pkg: {}'.format(pkg))
+    return pm.install_app(pkg, options, app_id=None)
+
+
+def uninstall_package(pm, package, app_id):
+    display.vvv("DC/OS: uninstalling package {}".format(package))
+    return pm.uninstall_app(package, remove_all=True, app_id=app_id)
+
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+        del tmp  # tmp no longer has any effect
+
+        if self._play_context.check_mode:
+            # in --check mode, always skip this module execution
+            result['skipped'] = True
+            result['msg'] = 'The dcos task does not support check mode'
+            return result
+
+        package_name = self._task.args.get('name', None)
+        package_version = self._task.args.get('version', None)
+        state = self._task.args.get('state', 'present')
+        app_id = self._task.args.get('app_id', package_name)
+        options = self._task.args.get('options') or {}
+        options['name'] = app_id
+
+        _ensure_dcos()
+
+        pm = dcos.package.get_package_manager()
+
+        current_version = get_current_version(pm, package_name, app_id)
+        wanted_version = get_wanted_version(package_version, state)
+
+        if current_version == wanted_version:
+            display.vvv("Package {} already in desired state".format(package_name))
+            result['changed'] = False
+        else:
+            display.vvv("Package {} not in desired state".format(package_name))
+            try:
+                if wanted_version is not None:
+                    install_package(pm, package_name, wanted_version, options)
+                    if wanted_version != get_current_version(pm, package_name, app_id):
+                        raise AnsibleActionFail('failed to install')
+                else:
+                    uninstall_package(pm, package_name, app_id)
+                    if wanted_version != get_current_version(pm, package_name, app_id):
+                        raise AnsibleActionFail('failed to uninstall')
+
+            except DCOSException as e:
+                #raise AnsibleActionFail(e)
+                raise
+
+            result['changed'] = True
+
+        return result
--- library/dcos_package.py
+++ library/dcos_package.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 ANSIBLE_METADATA = {
     'metadata_version': '0.1',
@@ -57,109 +58,109 @@ RETURN = '''
 nothing special
 '''
 
-from ansible.module_utils.basic import AnsibleModule
-
-try:
-    import dcos
-    import dcos.package
-    from dcos.errors import DCOSException
-    HAS_DCOS = True
-except ImportError:
-    HAS_DCOS = False
-
-from ansible.module_utils.six import PY2
-
-
-def _version(v):
-    return tuple(map(int, v.split('.')))
-
-
-def _ensure_dcos(module):
-    """Check whether the dcos[cli] package is installed."""
-    if PY2:
-        module.fail_json(msg="Python 3 is required for DC/OS")
-
-    if not HAS_DCOS:
-        module.fail_json(msg="`dcoscli` is not installed, but it is required "
-                             "for the Ansible dcos_package module")
-    else:
-        v = _version(dcos.version)
-        if v < (0, 5, 0):
-            module.fail_json(msg="dcos 0.5.x is required, found {}"
-                                 .format(dcos.version))
-        if v >= (0, 6, 0):
-            module.warn("dcos cli version > 0.5.x detected, may not work")
-
-
-def get_current_version(pm, package, app_id):
-    packages = {p['name']: p['version'] for p in pm.installed_apps(None, None)}
-    return packages.get(package)
-
-
-def get_wanted_version(version, state):
-    if state == 'absent':
-        return None
-    return version
-
-
-def install_package(pm, package, app_id, version, options=None):
-    print('DC/OS: installing package {} version {}'
-          .format(package, version))
-    pkg = pm.get_package_version(package, version)
-    return pm.install_app(pkg, options)
-
-
-def uninstall_package(pm, package, app_id):
-    print("DC/OS: uninstalling package {}".format(package))
-    return pm.uninstall_app(package, remove_all=True, app_id=app_id)
-
-
-def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            name=dict(type='str', required=True),
-            version=dict(type='str', default=None),
-            state=dict(type='str', choices=['absent', 'present'], default='present'),
-            app_id=dict(type='str', default=None),
-            options=dict(type='dict', default=None)
-        ),
-        supports_check_mode=True
-    )
-
-    _ensure_dcos(module)
-
-    result = dict(changed=False)
-
-    if module.check_mode:
-        return result
-
-    pm = dcos.package.get_package_manager()
-
-    package_name = module.params['name']
-    app_id = '/' + (module.params['app_id'] or package_name)
-    options = module.params['options'] or dict(name='app_id')
-    package_version = module.params['version']
-
-    current_version = get_current_version(pm, package_name, app_id)
-    wanted_version = get_wanted_version(package_version, module.params['state'])
-
-    if current_version != wanted_version:
-        try:
-            if wanted_version is not None:
-                install_package(pm, package_name, app_id, version, options)
-                if wanted_version != get_current_version(pm, package_name, app_id):
-                    module.fail_json('failed to install')
-            else:
-                uninstall_package(pm, package_name, app_id)
-                if wanted_version != get_current_version(pm, package_name, app_id):
-                    module.fail_json('failed to uninstall')
-        except DCOSException as e:
-            module.fail_json(str(e), **result)
-
-        result['changed'] = True
-
-    module.exit_json(**result)
-
-
-if __name__ == '__main__':
-    main()
+# from ansible.module_utils.basic import AnsibleModule
+#
+# try:
+#     import dcos
+#     import dcos.package
+#     from dcos.errors import DCOSException
+#     HAS_DCOS = True
+# except ImportError:
+#     HAS_DCOS = False
+#
+# from ansible.module_utils.six import PY2
+#
+#
+# def _version(v):
+#     return tuple(map(int, v.split('.')))
+#
+#
+# def _ensure_dcos(module):
+#     """Check whether the dcos[cli] package is installed."""
+#     if PY2:
+#         module.fail_json(msg="Python 3 is required for DC/OS")
+#
+#     if not HAS_DCOS:
+#         module.fail_json(msg="`dcoscli` is not installed, but it is required "
+#                              "for the Ansible dcos_package module")
+#     else:
+#         v = _version(dcos.version)
+#         if v < (0, 5, 0):
+#             module.fail_json(msg="dcos 0.5.x is required, found {}"
+#                                  .format(dcos.version))
+#         if v >= (0, 6, 0):
+#             module.warn("dcos cli version > 0.5.x detected, may not work")
+#
+#
+# def get_current_version(pm, package, app_id):
+#     packages = {p['name']: p['version'] for p in pm.installed_apps(None, None)}
+#     return packages.get(package)
+#
+#
+# def get_wanted_version(version, state):
+#     if state == 'absent':
+#         return None
+#     return version
+#
+#
+# def install_package(pm, package, app_id, version, options=None):
+#     print('DC/OS: installing package {} version {}'
+#           .format(package, version))
+#     pkg = pm.get_package_version(package, version)
+#     return pm.install_app(pkg, options)
+#
+#
+# def uninstall_package(pm, package, app_id):
+#     print("DC/OS: uninstalling package {}".format(package))
+#     return pm.uninstall_app(package, remove_all=True, app_id=app_id)
+#
+#
+# def main():
+#     module = AnsibleModule(
+#         argument_spec=dict(
+#             name=dict(type='str', required=True),
+#             version=dict(type='str', default=None),
+#             state=dict(type='str', choices=['absent', 'present'], default='present'),
+#             app_id=dict(type='str', default=None),
+#             options=dict(type='dict', default=None)
+#         ),
+#         supports_check_mode=True
+#     )
+#
+#     _ensure_dcos(module)
+#
+#     result = dict(changed=False)
+#
+#     if module.check_mode:
+#         return result
+#
+#     pm = dcos.package.get_package_manager()
+#
+#     package_name = module.params['name']
+#     app_id = '/' + (module.params['app_id'] or package_name)
+#     options = module.params['options'] or dict(name='app_id')
+#     package_version = module.params['version']
+#
+#     current_version = get_current_version(pm, package_name, app_id)
+#     wanted_version = get_wanted_version(package_version, module.params['state'])
+#
+#     if current_version != wanted_version:
+#         try:
+#             if wanted_version is not None:
+#                 install_package(pm, package_name, app_id, version, options)
+#                 if wanted_version != get_current_version(pm, package_name, app_id):
+#                     module.fail_json('failed to install')
+#             else:
+#                 uninstall_package(pm, package_name, app_id)
+#                 if wanted_version != get_current_version(pm, package_name, app_id):
+#                     module.fail_json('failed to uninstall')
+#         except DCOSException as e:
+#             module.fail_json(str(e), **result)
+#
+#         result['changed'] = True
+#
+#     module.exit_json(**result)
+#
+#
+# if __name__ == '__main__':
+#     main()
